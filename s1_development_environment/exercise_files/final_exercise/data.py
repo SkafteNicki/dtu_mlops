1a6bc2cd382af31f4bdb4905fbf241b433a64c30
abzink <s213027@student.dtu.dk>
2023-01-03 10:57:18 +0100

    change please

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,8 +1,16 @@
 import torch
-
+import numpy as np
 
 def mnist():
+    # open data from npz file
+    test_ims = torch.tensor(np.load(f'/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/test.npz')['images'])
+    test_labels = torch.tensor(np.load(f"/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/test.npz")["labels"])
+    train_ims = torch.tensor(np.array([np.load(f"/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/train_{i}.npz")["images"] for i in range(5)]).reshape(-1,28,28))
+    train_labels = torch.tensor(np.array([np.load(f"/Users/annabzinkowska/DTU/MLops/dtu_mlops/data/corruptmnist/train_{i}.npz")["labels"] for i in range(5)]).reshape(-1))
+    
     # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+    #train = torch.randn(50000, 784)
+    #test = torch.randn(10000, 784) 
+    return list(zip(train_ims, train_labels)), list(zip(test_ims, test_labels))
+
+
--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -7,6 +7,11 @@ import click
 from data import mnist
 from model import MyAwesomeModel
 
+from torch import nn, optim
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 @click.group()
 def cli():
@@ -19,10 +24,35 @@ def train(lr):
     print("Training day and night")
     print(lr)
 
-    # TODO: Implement training loop here
     model = MyAwesomeModel()
     train_set, _ = mnist()
+    trainloader = DataLoader(train_set, batch_size=64, shuffle=True)
 
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    
+    epochs = 200
+    epochs_loss = []
+    for _ in range(epochs):
+        running_loss = 0
+        for images, labels in trainloader:
+            # Flatten MNIST images into a 784 long vector
+            images = images.view(images.shape[0], -1)
+            optimizer.zero_grad() # reset gradients
+            output = model(images.float()) # run data through model
+            loss = criterion(output, labels) # calculate loss
+            loss.backward() # calculate gradient
+            optimizer.step() # back probagate
+            running_loss += loss.item() # calculate loss
+        else:
+            loss_epoch_mean = running_loss/len(trainloader)
+            epochs_loss.append(loss_epoch_mean)
+            print(f"Training loss: {running_loss/len(trainloader)}")
+
+
+    plt.plot(epochs_loss)
+    plt.show()
+    torch.save(model.state_dict(), "model_checkpoint.pt")
 
 @click.command()
 @click.argument("model_checkpoint")
@@ -30,9 +60,22 @@ def evaluate(model_checkpoint):
     print("Evaluating until hitting the ceiling")
     print(model_checkpoint)
 
-    # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
+    model = MyAwesomeModel()
+    model.load_state_dict(torch.load("model_checkpoint.pt"))
+
     _, test_set = mnist()
+    testloader = DataLoader(test_set, batch_size=len(test_set))
+    
+    model.eval()
+    with torch.no_grad():
+        for images, labels in testloader:
+            images = images.view(images.shape[0], -1)
+            ps = model(images.float())
+            _, top_class = ps.topk(1, dim=1)
+            top_class = torch.squeeze(top_class)
+            equals = top_class == labels.view(*top_class.shape)
+            accuracy = torch.mean(equals.type(torch.FloatTensor))
+            print(f"Accuracy: {accuracy.item()*100}%")          
 
 
 cli.add_command(train)
