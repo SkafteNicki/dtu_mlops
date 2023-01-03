9d5f35372fb33dafe7aab282f5ad21ab94511d18
Charidimos Vradis <haridimos9@gmail.com>
2023-01-03 10:39:59 +0100

    changing some spellings

--- s2_organisation_and_version_control/exercise_files/typing_exercise.py
+++ s2_organisation_and_version_control/exercise_files/typing_exercise.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+# smthing to have here
 
 class Network(nn.Module):
     def __init__(self, input_size, output_size, hidden_layers, drop_p=0.5):
