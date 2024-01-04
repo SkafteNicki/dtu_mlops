38868a425894c0a58383315520c7bc882f7dd34d
Diogo Adegas <diogoadegas@Diogos-MacBook-Pro.local>
2024-01-04 12:07:05 +0100

    final exercises for s1

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -3,7 +3,26 @@ import torch
 
 def mnist():
     """Return train and test dataloaders for MNIST."""
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784)
-    return train, test
+    train_data, train_labels = [ ], [ ]
+    for i in range(5):
+        train_data.append(torch.load(f"/Users/diogoadegas/Documents/Machine_Learning_Operations/dtu_mlops/data/corruptmnist/train_images_{i}.pt"))
+        train_labels.append(torch.load(f"/Users/diogoadegas/Documents/Machine_Learning_Operations/dtu_mlops/data/corruptmnist/train_target_{i}.pt"))
+
+    train_data = torch.cat(train_data, dim=0)
+    train_labels = torch.cat(train_labels, dim=0)
+
+    test_data = torch.load("/Users/diogoadegas/Documents/Machine_Learning_Operations/dtu_mlops/data/corruptmnist/test_images.pt")
+    test_labels = torch.load("/Users/diogoadegas/Documents/Machine_Learning_Operations/dtu_mlops/data/corruptmnist/test_target.pt")
+
+    print(train_data.shape)
+    print(train_labels.shape)
+    print(test_data.shape)
+    print(test_labels.shape)
+
+    train_data = train_data.unsqueeze(1)
+    test_data = test_data.unsqueeze(1)
+
+    return (
+        torch.utils.data.TensorDataset(train_data, train_labels), 
+        torch.utils.data.TensorDataset(test_data, test_labels)
+    )
--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -1,8 +1,10 @@
 import click
 import torch
-from model import MyAwesomeModel
+from torch import nn
+from model import myawesomemodel
 
 from data import mnist
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 @click.group()
@@ -13,14 +15,35 @@ def cli():
 
 @click.command()
 @click.option("--lr", default=1e-3, help="learning rate to use for training")
-def train(lr):
+@click.option("--batch_size", default=256, help="batch size to use for training")
+@click.option("--num_epochs", default=20, help="number of epochs to train for")
+def train(lr, batch_size, num_epochs):
     """Train a model on MNIST."""
     print("Training day and night")
     print(lr)
+    print(batch_size)
 
     # TODO: Implement training loop here
-    model = MyAwesomeModel()
+    model = myawesomemodel.to(device)
     train_set, _ = mnist()
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = nn.CrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        for batch in train_dataloader:
+            optimizer.zero_grad()
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch {epoch} Loss {loss}")
+
+    torch.save(model, "model.pt")
 
 
 @click.command()
@@ -33,6 +56,26 @@ def evaluate(model_checkpoint):
     # TODO: Implement evaluation logic here
     model = torch.load(model_checkpoint)
     _, test_set = mnist()
+    test_dataloader = torch.utils.data.DataLoader(
+        test_set, batch_size=64, shuffle=False
+    )
+    model.eval()
+
+    test_preds = [ ]
+    test_labels = [ ]
+    with torch.no_grad():
+        for batch in test_dataloader:
+            x, y = batch
+            x = x.to(device)
+            y = y.to(device)
+            y_pred = model(x)
+            test_preds.append(y_pred.argmax(dim=1).cpu())
+            test_labels.append(y.cpu())
+
+    test_preds = torch.cat(test_preds, dim=0)
+    test_labels = torch.cat(test_labels, dim=0)
+
+    print((test_preds == test_labels).float().mean())
 
 
 cli.add_command(train)
