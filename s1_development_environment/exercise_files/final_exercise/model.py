45bba247189e773a7f12c9b7468548f03e186e86
davidAlvaBro <davidalvabro@gmail.com>
2024-01-04 15:04:02 +0100

    work from day 1

--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,9 +1,12 @@
 from torch import nn
 
 
-class MyAwesomeModel(nn.Module):
-    """My awesome model."""
-
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(784, 128)
+myawesomemodel = nn.Sequential(
+    nn.Conv2d(1, 32, 3),  # [B, 1, 28, 28] -> [B, 32, 26, 26]
+    nn.LeakyReLU(),
+    nn.Conv2d(32, 64, 3), # [B, 32, 26, 26] -> [B, 64, 24, 24]
+    nn.LeakyReLU(),
+    nn.MaxPool2d(2),      # [B, 64, 24, 24] -> [B, 64, 12, 12]
+    nn.Flatten(),        # [B, 64, 12, 12] -> [B, 64 * 12 * 12]
+    nn.Linear(64 * 12 * 12, 10),
+)
