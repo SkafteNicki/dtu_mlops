0de6882db2e65b2d21f36c33a82c4ad53da26035
nagasawalin <shiezhenlin@gmail.com>
2024-01-12 15:23:16 +0100

    log configuration

--- s4_debugging_and_logging/exercise_files/my_logger.py
+++ s4_debugging_and_logging/exercise_files/my_logger.py
@@ -1,16 +1,14 @@
 import logging
 import sys
 
-# Create super basic logger
-logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
-logger = logging.getLogger(__name__) # 
+##NEW!!!!
+import logging.config
+from pathlib import Path
 
-# Logging levels (from lowest to highest priority)
-logger.debug("Used for debugging your code.")
-logger.info("Informative messages from your code.")
-logger.warning("Everything works but there is something to be aware of.")
-logger.error("There's been a mistake with the process.")
-logger.critical("There is something terribly wrong and process may terminate.")
+## NEW! Set up LOGS_DIR
+## REMEMBER TO ADD A RAW STRING "r" BEFORE ADDING THE DIC LOCATION, TO AVOID ISSUES WITH EXCAPE CHARACTERS
+LOGS_DIR = Path(r"D:\DTU\Study\2024Jan\MLOps\dtu_mlops\dtu_mlops\s4_debugging_and_logging\exercise_files")
+LOGS_DIR.mkdir(exist_ok=True)
 
 logging_config = {
     "version": 1,
@@ -50,3 +48,19 @@ logging_config = {
         "propagate": True,
     },
 }
+
+
+#APPLY logging configuration
+logging.config.dictConfig(logging_config)
+
+# Create super basic logger
+logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+logger = logging.getLogger(__name__) # 
+
+# Logging levels (from lowest to highest priority)
+logger.debug("Used for debugging your code.")
+logger.info("Informative messages from your code.")
+logger.warning("Everything works but there is something to be aware of.")
+logger.error("There's been a mistake with the process.")
+logger.critical("There is something terribly wrong and process may terminate.")
+
