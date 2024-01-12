532e1782ab216083f3aecad6a70114ffd80731a9
nagasawalin <shiezhenlin@gmail.com>
2024-01-12 15:20:06 +0100

    log configuration

--- s4_debugging_and_logging/exercise_files/my_logger.py
+++ s4_debugging_and_logging/exercise_files/my_logger.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 # Create super basic logger
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
 logger = logging.getLogger(__name__) # 
 
 # Logging levels (from lowest to highest priority)
@@ -11,3 +11,42 @@ logger.info("Informative messages from your code.")
 logger.warning("Everything works but there is something to be aware of.")
 logger.error("There's been a mistake with the process.")
 logger.critical("There is something terribly wrong and process may terminate.")
+
+logging_config = {
+    "version": 1,
+    "formatters": { # 
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(name)s:%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": { # 
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "root": {
+        "handlers": ["console", "info", "error"],
+        "level": logging.INFO,
+        "propagate": True,
+    },
+}
