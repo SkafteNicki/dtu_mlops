754e9be6228e022e3697e50c51efb394ab52cd95
Nicki Skafte <skaftenicki@gmail.com>
2024-01-14 14:36:36 +0100

    fix scraper

--- tools/repo_scraper/repo_scraper.py
+++ tools/repo_scraper/repo_scraper.py
@@ -156,9 +156,6 @@ def main(
         folder_name = repo.split("/")[-1]
         if clone_succes:
             os.system(f"cd {out_folder} && cp -r {folder_name} group_{group_nb} && rm -rf {folder_name}")
-            if folder_name in os.listdir(out_folder):  # copy may fail
-                shutil.rmtree(f"{out_folder}/{folder_name}")
-                clone_succes = False
         else:
             if folder_name in os.listdir(out_folder):
                 shutil.rmtree(f"{out_folder}/{folder_name}")
