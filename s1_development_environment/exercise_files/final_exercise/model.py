e8a920d34a2e757ee75431af0399c5210915773a
nagasawalin <shiezhenlin@gmail.com>
2024-01-15 10:40:50 +0100

    minor changes of s1_finalexe model.py

--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,5 +1,9 @@
 from torch import nn
 
+## my code below:
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
 
 class MyAwesomeModel(nn.Module):
     """My awesome model."""
@@ -7,3 +11,7 @@ class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(784, 128)
+        #my part below:
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
