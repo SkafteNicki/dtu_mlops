9d8677eb761f1acedc5d06eb354de5b1eb870564
Nicki Skafte <skaftenicki@gmail.com>
2024-06-04 14:07:13 +0200

    large wandb overhaul

---
+++ s4_debugging_and_logging/exercise_files/weights_and_bias_solution3.py
@@ -0,0 +1,96 @@
+import click
+import matplotlib.pyplot as plt
+import torch
+import wandb
+from my_project.data import corrupt_mnist
+from my_project.model import MyAwesomeModel
+from sklearn.metrics import RocCurveDisplay, accuracy_score, f1_score, precision_score, recall_score
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+
+@click.command()
+@click.option("--lr", type=float, default=0.001, help="Learning rate")
+@click.option("--batch_size", type=int, default=32, help="Batch size")
+@click.option("--epochs", type=int, default=5, help="Number of epochs")
+def train(lr, batch_size, epochs) -> None:
+    """Train a model on MNIST."""
+    print("Training day and night")
+    print(f"{lr=}, {batch_size=}, {epochs=}")
+    run = wandb.init(
+        project="corrupt_mnist",
+        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
+    )
+
+    model = MyAwesomeModel().to(DEVICE)
+    train_set, _ = corrupt_mnist()
+
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(epochs):
+        model.train()
+
+        preds, targets = [], []
+        for i, (img, target) in enumerate(train_dataloader):
+            img, target = img.to(DEVICE), target.to(DEVICE)
+            optimizer.zero_grad()
+            y_pred = model(img)
+            loss = loss_fn(y_pred, target)
+            loss.backward()
+            optimizer.step()
+            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+            wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+
+            preds.append(y_pred.detach().cpu())
+            targets.append(target.detach().cpu())
+
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+
+                # add a plot of the input images
+                images = wandb.Image(img[:5].detach().cpu(), caption="Input images")
+                wandb.log({"images": images})
+
+                # add a plot of histogram of the gradients
+                grads = torch.cat([p.grad.flatten() for p in model.parameters() if p.grad is not None], 0)
+                wandb.log({"gradients": wandb.Histogram(grads)})
+
+        # add a custom matplotlib plot of the ROC curves
+        preds = torch.cat(preds, 0)
+        targets = torch.cat(targets, 0)
+
+        for class_id in range(10):
+            one_hot = torch.zeros_like(targets)
+            one_hot[targets == class_id] = 1
+            _ = RocCurveDisplay.from_predictions(
+                one_hot,
+                preds[:, class_id],
+                name=f"ROC curve for {class_id}",
+                plot_chance_level=(class_id == 2),
+            )
+
+        wandb.plot({"roc": plt})
+        # alternative: wandb.log({"roc": wandb.plot.roc_curve(targets, preds)})
+
+    final_accuracy = accuracy_score(targets, preds.argmax(dim=1))
+    final_precision = precision_score(targets, preds.argmax(dim=1), average="weighted")
+    final_recall = recall_score(targets, preds.argmax(dim=1), average="weighted")
+    final_f1 = f1_score(targets, preds.argmax(dim=1), average="weighted")
+
+    # first we save the model to a file then log it as an artifact
+    torch.save(model.state_dict(), "model.pth")
+    artifact = wandb.Artifact(
+        name="corrupt_mnist_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": final_accuracy, "precision": final_precision, "recall": final_recall, "f1": final_f1},
+    )
+    artifact.add_file("model.pth")
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+    train()
