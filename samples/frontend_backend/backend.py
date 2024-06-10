a6c498acf4f083c7e4d0b9153a85e40799878a43
Nicki Skafte <skaftenicki@gmail.com>
2024-06-10 15:32:56 +0200

    add frontend backend app

---
+++ samples/frontend_backend/backend.py
@@ -0,0 +1,66 @@
+import json
+from contextlib import asynccontextmanager
+
+import torch
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from PIL import Image
+from torchvision import models, transforms
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Context manager to start and stop the lifespan events of the FastAPI application."""
+    global model, transform, imagenet_classes
+    # Load model
+    model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
+    model.eval()
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    imagenet_classes = json.load(open("imagenet-simple-labels.json"))
+
+    yield
+
+    # Clean up
+    del model
+    del transform
+    del imagenet_classes
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+def predict_image(image_path: str) -> str:
+    """Predict image class (or classes) given image path and return the result."""
+    img = Image.open(image_path).convert("RGB")
+    img = transform(img).unsqueeze(0)
+    with torch.no_grad():
+        output = model(img)
+    _, predicted_idx = torch.max(output, 1)
+    return output.softmax(dim=-1), imagenet_classes[predicted_idx.item()]
+
+
+@app.get("/")
+async def root():
+    """Root endpoint."""
+    return {"message": "Hello from the backend!"}
+
+
+# FastAPI endpoint for image classification
+@app.post("/classify/")
+async def classify_image(file: UploadFile = File(...)):
+    """Classify image endpoint."""
+    try:
+        contents = await file.read()
+        with open(file.filename, "wb") as f:
+            f.write(contents)
+        probabilities, prediction = predict_image(file.filename)
+        return {"filename": file.filename, "prediction": prediction, "probabilities": probabilities.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=500) from e
---
+++ samples/frontend_backend/frontend.py
@@ -0,0 +1,63 @@
+import os
+
+import pandas as pd
+import requests
+import streamlit as st
+from google.cloud import run_v2
+
+
+def get_backend_url():
+    """Get the URL of the backend service."""
+    parent = "projects/my-personal-mlops-project/locations/europe-west1"
+    client = run_v2.ServicesClient()
+    services = client.list_services(parent=parent)
+    for service in services:
+        if service.name.split("/")[-1] == "production-model":
+            return service.uri
+    name = os.environ.get("BACKEND", None)
+    return name
+
+
+def classify_image(image, backend):
+    """Send the image to the backend for classification."""
+    predict_url = f"{backend}/predict"
+    response = requests.post(predict_url, files={"image": image}, timeout=10)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def main():
+    """Main function of the Streamlit frontend."""
+    backend = get_backend_url()
+    if backend is None:
+        raise ValueError("Backend service not found")
+
+    st.title("Image Classification")
+
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = uploaded_file.read()
+        result = classify_image(image, backend=backend)
+
+        if result is not None:
+            prediction = result["prediction"]
+            probabilities = result["probabilities"]
+
+            # show the image and prediction
+            st.image(image, caption="Uploaded Image")
+            st.write("Prediction:", prediction)
+
+            # make a nice bar chart
+            data = {"Class": [f"Class {i}" for i in range(10)], "Probability": probabilities}
+            df = pd.DataFrame(data)
+            df.set_index("Class", inplace=True)
+            st.bar_chart(df, y="Probability")
+        else:
+            st.write("Failed to get prediction")
+
+
+if __name__ == "__main__":
+    main()
