17b10ed2aae17b382bd637199658841fb0677453
Nicki Skafte <skaftenicki@gmail.com>
2024-06-24 07:11:20 +0200

    delete file

--- s10_extra/exercise_files/calibration.py
+++
@@ -1,5 +0,0 @@
-def calculate_calibration_error(calibration_data, calibration_model):
-    # Calculate the error between the calibration data and the calibration model
-    error = calibration_data - calibration_model
-    return error
-
