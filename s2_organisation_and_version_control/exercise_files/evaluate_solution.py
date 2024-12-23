c8ffcf6c46950e0920e4564d21e5557f1854d175
SkafteNicki <SkafteNicki@users.noreply.github.com>
2024-12-23 13:45:58 +0000

    Pre-commit fixes

--- s2_organisation_and_version_control/exercise_files/evaluate_solution.py
+++ s2_organisation_and_version_control/exercise_files/evaluate_solution.py
@@ -1,8 +1,7 @@
 import torch
 import typer
-from model import MyAwesomeModel
-
 from data import corrupt_mnist
+from model import MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
