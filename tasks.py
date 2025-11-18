c36da2dff5f5b9e79f11b6361a8fe2903d792f3f
Nicki Skafte <skaftenicki@gmail.com>
2025-11-18 09:00:49 +0100

    move all requirements to uv

--- tasks.py
+++ tasks.py
@@ -10,36 +10,28 @@ WINDOWS = os.name == "nt"
 @task
 def install(ctx) -> None:
     """Create the environment for course."""
-    ctx.run("conda create -n dtumlops python=3.11 pip --no-default-packages -y", echo=True, pty=not WINDOWS)
-    ctx.run("conda activate dtumlops", echo=True, pty=not WINDOWS)
-    ctx.run("pip install -r requirements.txt", echo=True, pty=not WINDOWS)
-
-
-@task
-def requirements(ctx):
-    """Install Python requirements."""
-    ctx.run("pip install -r requirements.txt", echo=True, pty=not WINDOWS)
+    ctx.run("uv sync", echo=True, pty=not WINDOWS)
 
 
 @task
 def precommit(ctx) -> None:
     """Install and run pre-commit checks."""
-    ctx.run("pre-commit install", echo=True, pty=not WINDOWS)
-    ctx.run("pre-commit run --all-files", echo=True, pty=not WINDOWS)
+    ctx.run("uv run pre-commit install", echo=True, pty=not WINDOWS)
+    ctx.run("uv run pre-commit run --all-files", echo=True, pty=not WINDOWS)
 
 
 @task(aliases=["mkdocs"])
 def docs(ctx) -> None:
     """Build the documentation."""
-    ctx.run("mkdocs serve --dirty", echo=True, pty=not WINDOWS)
+    ctx.run("uv run mkdocs serve --dirty", echo=True, pty=not WINDOWS)
 
 
 @task
 def lint(ctx) -> None:
     """Run linters."""
-    ctx.run("ruff check . --fix", echo=True, pty=not WINDOWS, warn=True)
-    ctx.run("ruff format .", echo=True, pty=not WINDOWS, warn=True)
-    ctx.run("mypy .", echo=True, pty=not WINDOWS)
+    ctx.run("uv run ruff check . --fix", echo=True, pty=not WINDOWS, warn=True)
+    ctx.run("uv run ruff format .", echo=True, pty=not WINDOWS, warn=True)
+    ctx.run("uv run mypy .", echo=True, pty=not WINDOWS)
 
 
 @task
